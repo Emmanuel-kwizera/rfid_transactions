020f50de65b8045d764c1c7a8892b04599846730
Natusino <kwizemmy2004@gmail.com>
2021-05-04 15:59:07 +0200

    transaction model

---
+++ server/models/transaction.model.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const TransactionSchema = new Schema({
+    UUID: {
+        type:String,
+        required:[true,"UUID is required"],
+        minlength:[3,"UUID must be at least 3 letters "],
+        maxlength:[50,"UUID must not exceed 255 letters"],
+        trim:true
+    },
+    InitialBalance: {
+        type:Number,
+        required:[true,"InitialBalance is required"],
+        trim:true
+    },
+},
+    {
+
+      timestamps:{
+        createdAt:"Registered_at",
+        updatedAt:"Updated_at"
+      },
+      toJSON:{
+        virtuals:true
+      },
+      toObject:{
+        virtuals:true
+      }
+    }
+);
+
+module.exports = model("Transaction",TransactionSchema);
