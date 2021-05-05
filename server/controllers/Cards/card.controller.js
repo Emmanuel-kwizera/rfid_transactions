5fe7435a1021be1c57bef70c487165368d0c95e5
Natusino <kwizemmy2004@gmail.com>
2021-05-05 21:48:52 +0200

    adding update the card

--- server/controllers/Cards/card.controller.js
+++ server/controllers/Cards/card.controller.js
@@ -7,3 +7,10 @@ module.exports.addCard = asyncHandler(async(req,res,next) => {
     const newCard = await Card.create({UUID,Balance});
     if(newCard){res.send({success:true,data:newCard}).status(200);} else return next(new ErrorMessage("Something Went wrong during Card creation",400));
 })
+
+module.exports.updateCard = asyncHandler(async(req,res,next) => {
+    let card_id = req.params.id;
+    const newCard = {...req.body};
+    let card = await Card.findByIdAndUpdate(card_id,newCard,{ new:true});
+    if(card){res.send({success:true,data:card}).status(201);}else return next(new ErrorMessage("SOMETHING WENT WRONG WHILE UPDATING USER",400));
+})
