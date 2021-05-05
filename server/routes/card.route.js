132f08ede65914a2e857b0ce52a6cbc69ab1e341
Natusino <kwizemmy2004@gmail.com>
2021-05-05 21:43:09 +0200

    route added

--- server/routes/card.route.js
+++ server/routes/card.route.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const Router = express.Router();
+
+const { addCard }= require("../controllers/Cards/card.controller");
+
+Router.post("/add",addCard);
+
+module.exports =Router;
