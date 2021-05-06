cd54133be40aeb1107de8ee67441016cc632f5dd
Natusino <kwizemmy2004@gmail.com>
2021-05-06 20:56:05 +0200

    logo and container added

--- cli/src/components/Home.svelte
+++ cli/src/components/Home.svelte
@@ -5,15 +5,18 @@
 <Tailwind />
 
 <main>
-    <div class="w-1/2">
-        <p>Hello</p>
+    <div class="w-1/2 left-side-web">
+        <img class="w-64 m-auto py-24" src="https://o.remove.bg/downloads/79340d09-d394-4776-97b4-c9739b33fb25/279-2793698_rfid-card-reader-logo-removebg-preview.png" alt="card">
     </div>
 </main>
 
 <style>
     :global(body){
-		background-color: #b8c6db;
 		width: 100vw;
 		height: 100vh;
+    }
+    .left-side-web{
+        background-color: #ff6b22;
+        height: 100vh;
     }
 </style>
