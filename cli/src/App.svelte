b197a41a1de525386040c457a66cc735a32c3da0
Natusino <kwizemmy2004@gmail.com>
2021-05-06 20:43:11 +0200

    test done

--- cli/src/App.svelte
+++ cli/src/App.svelte
@@ -1,16 +1,10 @@
 <script>
   import Tailwind from './components/Tailwind.svelte';
-
-  export let name;
+  import Home from './components/Home.svelte'
 </script>
 
 <Tailwind />
 
-<main class="text-center p-4 max-w-xs mx-auto sm:max-w-none">
-  <h1 class="text-red-500 text-6xl uppercase leading-loose font-thin">Hello {name}!</h1>
-  <p class="text-gray-700">
-    Visit the <a class="text-blue-600 hover:underline" href="https://svelte.dev/tutorial"
-      >Svelte tutorial</a
-    > to learn how to build Svelte apps.
-  </p>
+<main>
+  <Home />
 </main>
--- cli/src/components/Home.svelte
+++ cli/src/components/Home.svelte
@@ -1,8 +1,19 @@
 <script>
-    import Tailwind from './components/Tailwind.svelte';
-  </script>
-  
-  <Tailwind />
-  
-  <main>
-  </main>
+    import Tailwind from './Tailwind.svelte';
+</script>
+
+<Tailwind />
+
+<main>
+    <div class="w-1/2">
+        <p>Hello</p>
+    </div>
+</main>
+
+<style>
+    :global(body){
+		background-color: #b8c6db;
+		width: 100vw;
+		height: 100vh;
+    }
+</style>
