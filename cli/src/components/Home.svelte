b639d7a367799d80f032fa2e4fd0e61a17b3ab93
Natusino <kwizemmy2004@gmail.com>
2021-05-06 21:40:47 +0200

    adding table on the right

--- cli/src/components/Home.svelte
+++ cli/src/components/Home.svelte
@@ -6,7 +6,15 @@
 
 <main>
     <div class="w-1/2 left-side-web">
-        <img class="w-64 m-auto py-24" src="https://o.remove.bg/downloads/79340d09-d394-4776-97b4-c9739b33fb25/279-2793698_rfid-card-reader-logo-removebg-preview.png" alt="card">
+        <img class="w-60 m-auto pt-32" src="https://o.remove.bg/downloads/79340d09-d394-4776-97b4-c9739b33fb25/279-2793698_rfid-card-reader-logo-removebg-preview.png" alt="card">
+        <div>
+            <h4 class="pl-48 text-white text-2xl font-serif">Welcome To Transacción fácil</h4>
+        </div>
+    </div>
+    <div class="w-1/2 Right-side-web bg-white">
+        <div class="sign-in-btn">
+        </div>
+
     </div>
 </main>
 
