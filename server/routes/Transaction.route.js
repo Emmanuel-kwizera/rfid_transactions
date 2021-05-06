5b181ed2774d4ffa400ae2be140ec19545385efd
Natusino <kwizemmy2004@gmail.com>
2021-05-06 08:48:41 +0200

    transaction and card updating done

--- server/routes/Transaction.route.js
+++ server/routes/Transaction.route.js
@@ -2,9 +2,11 @@ const express = require("express");
 const Router = express.Router();
 
 const {
-    addTransaction
+    addTransaction,
+    getTransactions
 } = require("../controllers/Transactions/transaction.controller");
 
 Router.post("/add",addTransaction);
+Router.get("/all", getTransactions);
 
 module.exports = Router;
